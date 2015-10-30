f29d3ea4137d9deb94a2fedb1d1269e39dfc92d7
Kavin Arasu <kavin@groupon.com>
2015-10-29 23:55:37 -0700

    Yelp table view cell

---
+++ YelpContentCell.h
@@ -0,0 +1,13 @@
+//
+//  YelpContentCell.h
+//  Yelp
+//
+//  Created by Kavin Arasu on 10/29/15.
+//  Copyright © 2015 codepath. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface YelpContentCell : UITableViewCell
+
+@end
---
+++ YelpContentCell.m
@@ -0,0 +1,23 @@
+//
+//  YelpContentCell.m
+//  Yelp
+//
+//  Created by Kavin Arasu on 10/29/15.
+//  Copyright © 2015 codepath. All rights reserved.
+//
+
+#import "YelpContentCell.h"
+
+@implementation YelpContentCell
+
+- (void)awakeFromNib {
+    // Initialization code
+}
+
+- (void)setSelected:(BOOL)selected animated:(BOOL)animated {
+    [super setSelected:selected animated:animated];
+
+    // Configure the view for the selected state
+}
+
+@end
